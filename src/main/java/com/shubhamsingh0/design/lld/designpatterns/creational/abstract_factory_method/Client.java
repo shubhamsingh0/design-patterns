f2deeffc6c72943126758360416e31fe87429eb7
shubham <shubham2singh0@gmail.com>
2025-08-05 10:47:50 +0530

    remove imports from abstract factory method's client

--- src/main/java/com/shubhamsingh0/design/lld/designpatterns/creational/abstract_factory_method/Client.java
+++ src/main/java/com/shubhamsingh0/design/lld/designpatterns/creational/abstract_factory_method/Client.java
@@ -1,7 +1,5 @@
 package com.shubhamsingh0.design.lld.designpatterns.creational.abstract_factory_method;
 
-import com.shubhamsingh0.design.lld.designpatterns.creational.factory_method.VehicleFactory;
-
 public class Client {
     public static void main(String[] args) {
 //     The abstract factory pattern is used to create families of related objects (like car parts) without specifying their concrete classes.
